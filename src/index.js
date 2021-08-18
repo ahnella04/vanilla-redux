07a28d49fb00c9409a5a84da300b654bd245dd46
ahnella04 <chqkq96@gmail.com>
2021-08-18 23:28:49 +0900

    third commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 let count = 0;
-
 number.innerText = count;
 
 const updateText = () => {
