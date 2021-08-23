6af20448ffc17b04e470e1c8d9f0bbcfc808a018
ahnella04 <chqkq96@gmail.com>
2021-08-23 11:30:44 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -5,21 +5,36 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
-    console.log(action);
+    console.log(count, action);
     if (action.type === "ADD") {
         return count + 1;
     } else if (action.type === "MINUS") {
         return count - 1;
+    } else {
+        return count;
     }
-    return count;
 };
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+    console.log("there was a change on the store");
+    console.log(countStore.getState());
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
